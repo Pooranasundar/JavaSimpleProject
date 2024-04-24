1de08feedee02305ee5b59623f8799efe0bbdbad
Pooranasundar <160992753+Pooranasundar@users.noreply.github.com>
2024-04-24 19:16:58 +0530

    Create sampleProject1.java

---
+++ sampleProject1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Hello
+{
+    Public static void main(String args[])
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your Name : ");
+  
+    String str = sc.nextLine();
+  
+    System.out.Println("Hello" +str);
+ }
+}
