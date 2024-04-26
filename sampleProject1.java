648076b8064a9e8def4d5563d3b42941049058e5
Pooranasundar <pooranasundar333@gmail.com>
2024-04-26 09:33:27 +0530

    Updated sampleProject1.java

--- sampleProject1.java
+++ sampleProject1.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class Hello
+public class sampleProject1
 {
-    Public static void main(String args[])
+    public static void main(String args[])
   {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter your Name : ");
   
     String str = sc.nextLine();
   
-    System.out.Println("Hello " +str);
+    System.out.println("Hello " +str);
  }
 }
